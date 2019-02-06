2f49c4f8a7cbaef1b98aa6be641e3e5dfc5c0309
lisandroosorio <47229362+lisandroosorio@users.noreply.github.com>
2019-02-05 22:59:19 -0500

    Add files via upload

---
+++ validate.php
@@ -0,0 +1,49 @@
+
+<?php
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+    include_once '../../config/Database.php';
+    include_once '../../models/Users.php';
+    
+    require __DIR__ . '/vendor/autoload.php';
+    use \Firebase\JWT\JWT;
+
+    // gets raw posted data
+    
+    $data = json_decode(file_get_contents("php://input"));
+    
+    
+    //try catch block for easy token checkup
+    try {
+        $secret = "onefishtwofish";
+        $authentication = JWT::decode($data->token, $secret, array('HS256'));
+        http_response_code(200);
+        echo json_encode(
+                         array(
+                               "message" => "login true",
+                               "data" => $authentication->data
+                               )
+                         );
+        
+    }
+    catch(Exception $e)
+    {
+        http_response_code(401);
+        echo json_encode(
+                         array(
+                               "message" => "fail",
+                               "error" => $e->getMessage()
+                               
+                               )
+                         
+                         
+                         
+                         );
+    }
+    
+    
+    
+

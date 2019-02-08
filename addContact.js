c6d1788ea8468e41ce6caae7ca05bd460f594a8d
BrandonGordon9 <46772452+BrandonGordon9@users.noreply.github.com>
2019-02-07 23:52:55 -0500

    update

--- addContact.js
+++ addContact.js
@@ -1,10 +1,11 @@
 var request = new XMLHttpRequest();
 
 request.open('POST', 'http://mistfiner.xyz/API-Files/api/Contact/addContact.php', true);
-request.setRequestHeader("Content", "Application/json");
+request.setRequestHeader("Content-Type", "application/json");
 
 request.onreadystatechange = function () {
     if (request.readyState === 4 && request.status === 200) {
+        
         var json = JSON.parse(request.responseText);
         console.log(json.FirstName + ", " + json.LastName + ", " + json.Email + ", " + json.Address + ", " + json.PhoneNumber + ", " + json.Users_isUsers);
     }

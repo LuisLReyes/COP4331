8a708f72bf6d3947b5fcb129dba326e44dbaa492
lisandroosorio <47229362+lisandroosorio@users.noreply.github.com>
2019-02-08 16:06:27 -0500

    Update login.php

--- API-Files/api/User/login.php
+++ API-Files/api/User/login.php
@@ -1,51 +1,53 @@
 
-<?php
+
+
+   <?php
     // Headers
-  
 
     header('Content-Type: application/json;charset=UTF-8');
    
     header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+    require __DIR__ . '/vendor/autoload.php';
+    //for json tokens
     
+  //  include_once 'config/Database.php';
+    //include_once 'models/Users.php';
+    use \Firebase\JWT\JWT;
     include_once '../../config/Database.php';
     include_once '../../models/Users.php';
-    
-    require __DIR__ . '/vendor/autoload.php';
-    use \Firebase\JWT\JWT;
-
     // Instantiate DB & connect
     
     $database = new database();
     $db = $database->connect();
     // Instantiate user
-    $user = new Users($db);
-    
+    $user = new user($db);
+
     //gets raw posted data
     $data = json_decode(file_get_contents("php://input"));
     
     $user->UserName = $data->UserName;
     $emailState = $user->email();
     
-    
+
     if($emailState && password_verify($data->passwordHash, $user->passwordHash))
     {
         $secret = "onefishtwofish";
         $issue = array(
-                       "iss" => 'http://www.arkto.xyz/', //issuer
-                       "exp" => time() + 3600,    //time to expire
-                       "data"=>array(
-                                     "idUsers" => $user->idUsers,
-                                     "FirstName" => $user->FirstName,
-                                     "LastName" => $user->LastName,
-                                     "UserName" => $user->UserName
-                                     
-                                     )//2d array component that holds data for us
+             "iss" => 'http://www.arkto.xyz/', //issuer
+             "exp" => time() + 3600,    //time to expire
+             "data"=>array(
+                                  "idUsers" => $user->idUsers,
+                                  "FirstName" => $user->FirstName,
+                                  "LastName" => $user->LastName,
+                                  "UserName" => $user->UserName
+                                  
+                                  )//2d array component that holds data for us
+            
+        
                        
-                       
-                       
-                       );
+        );
         $token = JWT::encode($issue,$secret);//make the token
         echo json_encode( array("message" => "login true","token"=>$token));
         

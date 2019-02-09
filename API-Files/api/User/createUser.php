9a4a5ff9e2df0f39c383230bb713b2cb13d735b2
lisandroosorio <47229362+lisandroosorio@users.noreply.github.com>
2019-02-09 17:03:48 -0500

    Update createUser.php

--- API-Files/api/User/createUser.php
+++ API-Files/api/User/createUser.php
@@ -9,28 +9,38 @@
     include_once '../../config/Database.php';
     include_once '../../models/Users.php';
 
+ 
+   require __DIR__ . '/vendor/autoload.php';
+     use \Firebase\JWT\JWT;
+    // Instantiate DB & connect
     // Instantiate DB & connect
     $database = new Database();
     $db = $database->connect();
-
     // Instantiate user
     $user = new Users($db);
-
     // Get raw posted data
     $data = json_decode(file_get_contents("php://input"));
-
+    
+    
     $user->FirstName = $data->FirstName;
     $user->LastName = $data->LastName;
     $user->UserName = $data->UserName;
     $user->passwordHash = $data->passwordHash;
+    $emailState = $user->email();
     // Create user
-    if($user->create()){
-            http_response_code(200);
+    if($emailState)
+    {
+        http_response_code(400);
+        echo json_encode(array('message' => 'User already in file')
+                         );
+    }
+   else if($user->create()){
+        http_response_code(200);
         echo json_encode(array('message' => 'User Created')
-        );
+                         );
     }
     else {
         http_response_code(400);
         echo json_encode(array('message' => 'User Not Created')
-        );
+                         );
     }

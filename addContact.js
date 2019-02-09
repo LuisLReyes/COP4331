aff16643f0e848eaaa883f50c6da433f3a5fb431
BrandonGordon9 <46772452+BrandonGordon9@users.noreply.github.com>
2019-02-09 16:42:51 -0500

    addcontact

--- addContact.js
+++ addContact.js
@@ -1,24 +1,42 @@
-var request = new XMLHttpRequest();
+function submitContact() {
 
-request.open('POST', 'https://www.hammerfall.xyz/API-Files/api/Contact/addContact.php', true);
-request.setRequestHeader("Content-Type", "application/json");
+var firstName = document.forms["myForm"]["firstName"].value;
+var lastName = document.forms["myForm"]["lastName"].value;
+var email = document.forms["myForm"]["email"].value;
+var address = document.forms["myForm"]["address"].value;
+var phone = document.forms["myForm"]["phone"].value;
 
-request.onreadystatechange = function () {
-    if (request.readyState === 4 && request.status === 200) {
+var contact_data=JSON.stringify({    "FirstName": firstName,
+                                     "LastName": lastName,
+                                     "Email": email,
+                                     "Address": address,
+                                     "PhoneNumber": phone,
+                                     "Users_idUsers": 5
+                                });
+console.log(contact_data);
 
-        var json = JSON.stringify(request.responseText);
-        console.log(json.inputFirstName + ", " + json.inputLastName + ", " + json.inputEmail + ", " + json.inputAddress + ", " + json.inputPhoneNumber + ", " + json.Users_isUsers);
-         
-    }
-};
 
-var data = JSON.stringify({"FirstName": "yo",
-                           "LastName": "yo",
-                           "Email": "asdf@gmail.com",
-                           "Address": "c",
-                           "PhoneNumber": 1234323,
-                           "Users_idUsers": 5}); 
-
-console.log(data);
-                   
-request.send(data);                            
+$.ajax({
+   url: "https://www.hammerfall.xyz/API-Files/api/Contact/addContact.php",
+   type : "POST",
+   contentType : 'application/json',
+   data : contact_data,
+   success : function(result){
+   console.log("adding contact");
+   // store jwt to cookie
+ //  setCookie("token", result.token, 1);
+   
+   // show home page & tell the user it was a successful login
+ //  showHomePage();
+   alert("CONTACT CREATE SUCCSSFULLL!!");
+   location = 'contacts.html';
+   
+   },
+   error: function(xhr, resp, text){
+   console.log("NOPE!");
+   // on error, tell the user login has failed & empty the input boxes
+   $('#response').html("<div class='alert alert-danger'>Adding Contact failed.</div>");
+   
+   }
+   });                           
+}

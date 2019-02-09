e726dc88ce1b7943f0605f3c630ca4c1fd42e517
BrandonGordon9 <46772452+BrandonGordon9@users.noreply.github.com>
2019-02-08 19:04:34 -0500

    update

--- addContact.js
+++ addContact.js
@@ -5,20 +5,20 @@ request.setRequestHeader("Content-Type", "application/json");
 
 request.onreadystatechange = function () {
     if (request.readyState === 4 && request.status === 200) {
-        
-        var json = JSON.parse(request.responseText);
-        console.log(json.FirstName + ", " + json.LastName + ", " + json.Email + ", " + json.Address + ", " + json.PhoneNumber + ", " + json.Users_isUsers);
-                 
+
+        var json = JSON.stringify(request.responseText);
+        console.log(json.inputFirstName + ", " + json.inputLastName + ", " + json.inputEmail + ", " + json.inputAddress + ", " + json.inputPhoneNumber + ", " + json.Users_isUsers);
+         
     }
 };
 
-var data = JSON.stringify({"FirstName": "ok",
-                           "LastName": "Name",
-                           "Email": "asdf@gmail.com",
-                           "Address": "1234 Street St.",
-                           "PhoneNumber": 1234323,
-                           "Users_idUsers": 5});
+var data = JSON.stringify({"FirstName": inputFirstName,
+                           "LastName": inputLastName,
+                           "Email": inputEmail,
+                           "Address": inputEmail,
+                           "PhoneNumber": inputPhoneNumber,
+                           "Users_idUsers": 5}); 
 
-            console.log(data);
+console.log(data);
                    
 request.send(data);                            
--- contacts.js
+++ contacts.js
@@ -5,7 +5,7 @@ request.onload = function () {
 
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
-  console.log(JSON.parse(this.response));
+  //console.log(JSON.parse(this.response));
   if (request.status >= 200 && request.status < 400) {
     data.data.forEach(Contact => {
       

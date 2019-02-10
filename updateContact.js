166446605987464fbdef4c3a6a08b418d7e9110d
BrandonGordon9 <46772452+BrandonGordon9@users.noreply.github.com>
2019-02-10 02:16:24 -0500

    update

--- updateContact.js
+++ updateContact.js
@@ -1,24 +1,37 @@
-var request = new XMLHttpRequest();
-
-request.open('POST', 'http://mistfiner.xyz/API-Files/api/Contact/updateContact.php', true);
-request.setRequestHeader("Content-Type", "application/json");
-
-request.onreadystatechange = function () {
-    if (request.readyState === 4 && request.status === 200) {
- 
-        var json = JSON.stringify(request.responseText);
-        console.log(json.inputFirstName + ", " + json.inputLastName + ", " + json.inputEmail + ", " + json.inputAddress + ", " + json.inputPhoneNumber + ", " + json.Users_isUsers);
-         
-    }
-};
-
-var data = JSON.stringify({"FirstName": inputFirstName,
-                           "LastName": inputLastName,
-                           "Email": inputEmail,
-                           "Address": inputEmail,
-                           "PhoneNumber": inputPhoneNumber,
-                           "Users_idUsers": 5}); 
-
-console.log(data);
-                   
-request.send(data);                            
+function editContact() {
+    var request = new XMLHttpRequest();
+    
+    request.open('PUT', 'https://www.hammerfall.xyz/API-Files/api/Contact/updateContact.php', true);
+    request.setRequestHeader("Content-Type", "application/json");
+    
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+     
+            var json = JSON.stringify(request.responseText);
+            //console.log(json.inputFirstName + ", " + json.inputLastName + ", " + json.inputEmail + ", " + json.inputAddress + ", " + json.inputPhoneNumber + ", " + json.Users_isUsers);
+             
+        }
+    };
+    
+    /*var teststring1 = document.forms["myForm"]["firstName"].value;
+    var teststring2 = document.forms["myForm"]["lastName"].value;
+    var teststring3 = document.forms["myForm"]["email"].value;
+    var teststring4 = document.forms["myForm"]["address"].value;
+    var teststring5 = document.forms["myForm"]["phone"].value; 
+    
+    var editeddata = {"FirstName": teststring1,
+                        "LastName": teststring2,
+                        "Email": teststring3,
+                        "Address": teststring4,
+                        "PhoneNumber": teststring5,
+                        "idContacts" : 2}; */
+    
+    console.log(editeddata);
+    
+    var data = JSON.stringify(prepareddata); 
+    
+    
+    console.log(data);
+                       
+    request.send(data); 
+    }                             

7bceb2fd7155dadf27e983ef6163d2dac8beddca
lisandroosorio <47229362+lisandroosorio@users.noreply.github.com>
2019-02-10 17:54:26 -0500

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -15,14 +15,15 @@ function loadContacts(cookieData){
 
         var div = document.getElementById('myTable');
         var id = JSON.stringify(Contact.idContacts).replace(/['"]+/g, '');
+                                                              
         var td1 = '<td id="FirstName'+id+'>'+JSON.stringify(Contact.FirstName)+'</td>';
         var td2 = '<td id="LastName'+id+'>'+JSON.stringify(Contact.LastName)+'</td>';
         var td3 = '<td id="Address'+id+'>'+JSON.stringify(Contact.Address)+'</td>';
         var td4 = '<td id="PhoneNumber'+id+'>'+JSON.stringify(Contact.PhoneNumber)+'</td>';
         var td5 = '<td id="Email'+id+'>'+JSON.stringify(Contact.Email)+'</td>';
         var deletebutton = '<td><button type="button" class="close" aria-label="Close" onclick="return deleteContact('+id+')"><span aria-hidden="true">&times;</span></button></td>';
-        var editbutton = '<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editModal" id="button-style">Edit</button></td>';
-
+        var editbutton = '<td><button type="button" class="btn btn-primary" data-toggle="modal"  data-target="#editModal" id="button-style">Update</button></td>';
+        //we need a way to send the id value we are getting from the contacts.js load contacts request just like you are doing with the delete button over to the 
         td1 = td1.replace(/['"]+/g, '');
         td2 = td2.replace(/['"]+/g, '');
         td3 = td3.replace(/['"]+/g, '');

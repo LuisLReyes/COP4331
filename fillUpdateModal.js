2a8c3a12bfe35f05c7e65c9eee02d9a7018c5e33
LuisLReyes <43256028+LuisLReyes@users.noreply.github.com>
2019-02-10 22:06:08 -0500

    Edit Modal Complete

--- fillUpdateModal.js
+++ fillUpdateModal.js
@@ -1,7 +1,8 @@
 function fillUpdateModal(passedId){
-    document.forms["editForm"]["editFirstName"].value = document.getElementById("contactRow" + passedId).childNodes[0];
-    document.forms["editForm"]["editLastName"].value = document.getElementById("contactRow" + passedId).childNodes[1];
-    document.forms["editForm"]["editAddress"].value = document.getElementById("contactRow" + passedId).childNodes[2];
-    document.forms["editForm"]["editPhoneNumber"].value = document.getElementById("contactRow" + passedId).childNodes[3];
-    document.forms["editForm"]["editEmail"].value = document.getElementById("contactRow" + passedId).childNodes[4];
+    console.log(document.getElementById("contactRow" + passedId).childNodes[0].textContent);
+    document.forms["editForm"]["editFirstName"].value = document.getElementById("contactRow" + passedId).childNodes[0].textContent
+    document.forms["editForm"]["editLastName"].value = document.getElementById("contactRow" + passedId).childNodes[1].textContent
+    document.forms["editForm"]["editAddress"].value = document.getElementById("contactRow" + passedId).childNodes[2].textContent
+    document.forms["editForm"]["editPhoneNumber"].value = document.getElementById("contactRow" + passedId).childNodes[3].textContent
+    document.forms["editForm"]["editEmail"].value = document.getElementById("contactRow" + passedId).childNodes[4].textContent
 }

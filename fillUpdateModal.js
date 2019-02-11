265d3d2db9df6130d5c0fe2e0800be3f2386d625
LuisLReyes <43256028+LuisLReyes@users.noreply.github.com>
2019-02-10 21:55:10 -0500

    fillupdatemodal

--- fillUpdateModal.js
+++ fillUpdateModal.js
@@ -1,3 +1,7 @@
-function fillUpdateModal(){
-    document.forms["editForm"]["editFirstName"].value = "asdf";
+function fillUpdateModal(passedId){
+    document.forms["editForm"]["editFirstName"].value = document.getElementById("contactRow" + passedId).childNodes[0];
+    document.forms["editForm"]["editLastName"].value = document.getElementById("contactRow" + passedId).childNodes[1];
+    document.forms["editForm"]["editAddress"].value = document.getElementById("contactRow" + passedId).childNodes[2];
+    document.forms["editForm"]["editPhoneNumber"].value = document.getElementById("contactRow" + passedId).childNodes[3];
+    document.forms["editForm"]["editEmail"].value = document.getElementById("contactRow" + passedId).childNodes[4];
 }
